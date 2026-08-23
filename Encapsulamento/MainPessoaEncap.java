7f0fe69f68ac01a8d948ac323089494e89908a3b
JoaoMarcosFMacedo <104098877+JoaoMarcosFMacedo@users.noreply.github.com>
2026-08-23 16:01:05 +0000

    A2

--- Encapsulamento/MainPessoaEncap.java
+++ Encapsulamento/MainPessoaEncap.java
@@ -2,23 +2,42 @@ import java.util.Scanner;
 
 public class MainPessoaEncap{
 
-    public Scanner s = new Scanner(System.in);
+    public static Scanner scan = new Scanner(System.in);
+    public static final int TAM = 5;
     public static void main(String []args){
         Pessoa p = new Pessoa("Marcos", 21);
-        Pessoa p2 = null;
+        Pessoa p2[] = new Pessoa[TAM];
+        
 
         System.out.printf("\n Meu nome é { %s } \nSua idade é { %d } ", p.getNome(), p.getIdade());
 
         for(int i = 0 ; i < 3; i +=1){
 
-            String n = s.nextLine();
-
+            System.out.printf("\n Nome %d : ", i);
+            String n = scan.nextLine();
             
+            
+            System.out.printf("\n Idade %d :", i);
+            int idade = scan.nextInt();
+
+            scan.nextLine();
+
+            p2[i] = new Pessoa(n, idade);
 
         }
 
-        
+        public void PrintPessoa(int TAm){
+            for(int i = 0; i < TAM ; i+= 1){
 
+                System.out.printf("====Pessoa %d====",i);
+
+
+
+            }
+        }
+        System.out.printf("\nIdade da pessoa 2 { %d } e o nome { %s }",p2[0].getIdade(), p2[0].getNome());
+        System.out.printf("\nIdade da pessoa 2 { %d } e o nome { %s }",p2[2].getIdade(), p2[2].getNome());
+        System.out.printf("\nIdade da pessoa 2 { %d } e o nome { %s }",p2[1].getIdade(), p2[1].getNome());
 
         
 
